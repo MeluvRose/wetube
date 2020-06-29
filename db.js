8a353978df70cbda5046ba56833a366c38dc4c78
MeluvRose <50815599+MeluvRose@users.noreply.github.com>
2020-06-29 17:01:45 +0900

    before setting mongodb

---
+++ db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, 
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB😊")
+const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
+
+
+db.once("open", handleOpen);
+db.on("error", handleError);
